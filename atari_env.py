af655a038df0e09196d42c879d9b090fbf7828f5
Kuilin Chen <alexchen199023@gmail.com>
2020-04-18 18:27:54 -0400

    test atari env

--- atari_env.py
+++ atari_env.py
@@ -1,7 +1,15 @@
-from tianshou.env.atari import create_atari_environment
+from envs.atari import AtariGame
 
-env = create_atari_environment(
-        'Pong', sticky_actions=False, max_episode_steps=2000)
+
+# %%
+env_id = 'BreakoutNoFrameskip-v4'
+env = AtariGame(env_id)
 state = env.reset()
-action = env.action_space().sample()
-next_state, reward, done, info = env.step(action)
+
+i = 0
+while True:
+    print(i)
+    action = env.action_space.sample()
+    next_state, reward, done, info = env.step(action)
+    if done:
+        break
