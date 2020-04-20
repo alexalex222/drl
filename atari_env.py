686e930a016b637c00b71570fa87284b84dfdefd
Kuilin Chen <alexchen199023@gmail.com>
2020-04-20 10:05:36 -0400

    test atari env

--- atari_env.py
+++ atari_env.py
@@ -8,8 +8,9 @@ state = env.reset()
 
 i = 0
 while True:
-    print(i)
+    i = i + 1
     action = env.action_space.sample()
     next_state, reward, done, info = env.step(action)
+    print(f'Step {i}, Action {action}, Reward {reward}')
     if done:
         break
