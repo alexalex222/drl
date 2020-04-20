b404b4c7375a1598f6f9d5dcdb02f9d3041ca9ff
Kuilin Chen <alexchen199023@gmail.com>
2020-04-20 11:27:34 -0400

    get and show results

--- plot_results.py
+++ plot_results.py
@@ -8,27 +8,28 @@ matplotlib.rcParams['figure.dpi'] = 200
 plt.style.use('ggplot')
 
 # %%
-env_id = 'CartPole-v0'
-gp_dyna_results = pd.read_csv('results/logs/gp_dyna_q_{}_run_5.csv'.format(env_id))
-ddqn_results = pd.read_csv('results/logs/ddqn_{}_run_5.csv'.format(env_id))
+task = 'CartPole'
+min_reward = 0
+max_reward = 200
+dqn_rewards = pd.read_csv('results/logs/dqn_{0}.csv'.format(task), sep=',', header=None).values
+dqn_rewards_mean = dqn_rewards.mean(axis=1)
+dqn_rewards_std = dqn_rewards.std(axis=1)
+dqn_rewards_upper = np.clip(dqn_rewards_mean + dqn_rewards_std, min_reward, max_reward)
+dqn_rewards_lower = np.clip(dqn_rewards_mean - dqn_rewards_std, min_reward, max_reward)
+
 
 fig, ax = plt.subplots()
-ax.plot(gp_dyna_results['episodes'], gp_dyna_results['rewards'],
-        color="#0072B2", label='Env Model Acceleration')
-ax.fill_between(gp_dyna_results['episodes'],
-                gp_dyna_results['rewards'] + 2*np.sqrt(gp_dyna_results['variances']),
-                gp_dyna_results['rewards'] - 2*np.sqrt(gp_dyna_results['variances']),
+ax.plot(dqn_rewards_mean,
+        color="#0072B2", label='DQN')
+ax.fill_between(np.arange(len(dqn_rewards_mean)),
+                dqn_rewards_upper,
+                dqn_rewards_lower,
                 facecolor="#0072B2", alpha=0.2)
-ax.plot(ddqn_results['episodes'], ddqn_results['rewards'],
-        color="#D55E00", label='DQN')
-ax.fill_between(ddqn_results['episodes'],
-                ddqn_results['rewards'] + 2*np.sqrt(ddqn_results['variances']),
-                ddqn_results['rewards'] - 2*np.sqrt(ddqn_results['variances']),
-                facecolor="#D55E00", alpha=0.2)
+
 ax.legend()
 ax.set_xlabel('Episode')
 ax.set_ylabel('Reward')
-ax.set_xlim([0, 500])
-ax.set_title(env_id)
+#ax.set_xlim([0, 500])
+ax.set_title(task)
 plt.tight_layout()
 plt.show()
--- read_tensorboard.py
+++ read_tensorboard.py
@@ -1,29 +1,30 @@
 # %%
-import os
-import pandas as pd
+import glob
 import numpy as np
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 
 # %%
-tb_path = 'C:\\repository\\drl\\tf_log'
-tb_name = 'dqn_CartPole-v0_2020-04-17_11-08-25'
-ea = EventAccumulator(os.path.join(tb_path, tb_name)).Reload()
+task = 'CartPole'
+tb_path = 'C:\\temp\\tb_log\\*{0}*'.format(task)
+all_rewards = []
+for tb_file in glob.glob(tb_path):
+    print(tb_file)
+    ea = EventAccumulator(tb_file).Reload()
 
-tags = ea.Tags()['scalars']
+    tags = ea.Tags()['scalars']
+
+    tag = 'Episode_Reward'
+    tag_values = []
+
+    for event in ea.Scalars(tag):
+        tag_values.append(event.value)
+
+    all_rewards.append(tag_values)
 
 
-tag = 'Episode_Reward'
-tag_values = []
-steps = []
-
-for event in ea.Scalars(tag):
-    tag_values.append(event.value)
-    steps.append(event.step)
-
-df = pd.DataFrame(data=np.array([steps, tag_values]).transpose(),
-                  columns=['steps', 'value'])
-
+all_rewards = np.asarray(all_rewards).transpose()
 csv_path = 'C:\\repository\\drl\\results\\logs\\'
-csv_file_name = tb_name + '_' + tag + '.csv'
-a = csv_path + csv_file_name
-df.to_csv(csv_path + csv_file_name, index=False)
+csv_file_name = task + '_temp' + '.csv'
+np.savetxt(csv_path + csv_file_name, all_rewards, delimiter=',')
+
+
